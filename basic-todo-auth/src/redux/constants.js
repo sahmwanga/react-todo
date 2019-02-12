b8ecd2e345f66c284b04d8d497b0bcffce98a6f8
salehe <sahmwanga@gmail.com>
2019-02-12 10:29:19 +0300

    add basic-todo-auth

---
+++ basic-todo-auth/src/redux/constants.js
@@ -0,0 +1,6 @@
+const constants = {
+  ADD_TODO: 'ADD_TOD',
+  REMOVE_TODO: 'REMOVE_TODO'
+}
+
+export default constants;
---
+++ basic-todo-auth/src/redux/reducers/todo.js
@@ -0,0 +1,27 @@
+// import c from '../constants'
+// import { combineReducers } from 'redux'
+
+// const initialState = [
+//   'test', 'abc'
+// ]
+
+// const todoReducer = (state = initialState, action) => {
+//   switch (action.type) {
+
+//     case c.ADD_TODO:
+//       console.log('add todo..')
+//       return state.concat(action.payload);
+
+//     case c.REMOVE_TODO:
+//       console.log('remove todo...')
+//       state = state.slice();
+//       state.splice(action.payload, 1)
+//       return state;
+
+//     default:
+//       return state;
+//   }
+
+// }
+
+// export default todoReducer
